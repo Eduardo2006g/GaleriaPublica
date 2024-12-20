9b38a137725cfc9e051232c6a7281de86693c0c4
Eduardo2006g <101937977+Eduardo2006g@users.noreply.github.com>
2024-12-20 08:57:04 -0300

    Add files via upload

---
+++ app/src/main/java/alvarenga/brandao/galeriapublica/GridViewFragment.java
@@ -0,0 +1,60 @@
+package alvarenga.brandao.galeriapublica;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.paging.PagingData;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link GridViewFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class GridViewFragment extends Fragment {
+
+    private MainViewModel mViewModel;
+    private View view;
+
+    public static GridViewFragment newInstance() {
+        return new GridViewFragment();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_grid_view, container, false);
+        // Inflate the layout for this fragment
+        return view;
+    }
+
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mViewModel = new ViewModelProvider(getActivity()).get(MainViewModel.class);
+        GridAdapter gridAdapter = new GridAdapter(new ImageDataComparator());
+        LiveData<PagingData<ImageData>> liveData = mViewModel.getPageLv();
+        liveData.observe(getViewLifecycleOwner(), new Observer<PagingData<ImageData>>() {
+            @Override
+            public void onChanged(PagingData<ImageData> imageDataPagingData) {
+                gridAdapter.submitData(getViewLifecycleOwner().getLifecycle(), imageDataPagingData);
+            }
+        });
+
+        RecyclerView rvGallery = (RecyclerView)view.findViewById(R.id.rvGrid);
+        rvGallery.setAdapter(gridAdapter);
+        float w = getResources().getDimension(R.dimen.im_width);
+        int numberOfColumns = Util.calculateNoOfColumns(getContext(), w);
+        rvGallery.setLayoutManager(new GridLayoutManager(getContext(),numberOfColumns));
+    }
+}
